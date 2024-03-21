f67a8fa436239449652f50dd2e430dd5beb4a78b
noelowaga <obrightonkabarak.ac.ke>
2024-03-21 17:24:45 +0300

    made changes to index.htm

--- authentication/views.py
+++ authentication/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
 # Create your views here.
 def home(request):
     return render(request, "authentication/index.html")
@@ -29,7 +31,23 @@ def signup(request):
     return render(request, "authentication/signup.html")
 
 def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+        
+        user = authenticate(username=username, password=pass1)
+        
+        if user is not None:
+            login(request, user)
+            fname = user.first_name
+            return render(request, "authentication/index.html", {'fname': fname})
+        else:
+            messages.error(request, "Invalid username or password.")
+            return render('home')    
+        
     return render(request, "authentication/signin.html")
 
 def signout(request):
-    pass
+    logout(request)
+    messages.success(request, "Logged Out Successfully!")
+    return redirect('home')
